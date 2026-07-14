4da8f6a961dce354218321c9b6bcca3227da3e4a
Rajawiw790 <108253254+Rajawiw790@users.noreply.github.com>
2026-07-14 11:37:17 +0100

    Delete src directory

--- src/database/models/dutyConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = new mongoose.Schema({
-    Guild: String,
-    Channel: String,
-    Message: String,
-});
-
-module.exports = mongoose.model("dutyConfig", Schema);
--- src/database/models/whitelistConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = new mongoose.Schema({
-    Guild: String,
-    ReviewChannel: String,
-    Role: String,
-});
-
-module.exports = mongoose.model("whitelistConfig", Schema);
